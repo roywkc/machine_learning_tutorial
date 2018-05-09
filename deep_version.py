f33132be650823965345762a6ed061af15c96402
roy cheung <roy@shoplineapp.com>
2018-05-09 12:05:04 +0800

    add some version checking files

---
+++ deep_version.py
@@ -0,0 +1,29 @@
+
+
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
+1
+2
+3
+4
+5
+6
+7
+8
+9
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
